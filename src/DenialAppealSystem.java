408f21deaba00f3f6a0e70661f682802a090a1e4
slavrinc <57003012+slavrinc@users.noreply.github.com>
2022-07-24 13:35:29 -0400

    Update DenialAppealSystem.java

--- src/DenialAppealSystem.java
+++ src/DenialAppealSystem.java
@@ -627,9 +627,6 @@ public class DenialAppealSystem extends javax.swing.JFrame {
      * - If function returns false, call connErr.setVisible(true); to notify the user that
      *   the connection could not be established.
      * 
-     * Part 2:
-     * - create a function in the controller class that requests a query from the db to generate the denialList
-     * 
      */
     private void LoginButtonClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_LoginButtonClicked
 
